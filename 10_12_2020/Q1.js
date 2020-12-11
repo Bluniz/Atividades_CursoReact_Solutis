1b74d255f799b90aaa6de2f37099fce7ed816215
Lucas Rosa <lucassantosrosa51@gmail.com>
2020-12-11 14:39:23 -0300

    Correção

--- 10_12_2020/Q1.js
+++ 10_12_2020/Q1.js
@@ -1,7 +1,11 @@
 const values = [1, 2, 8, 10, 50, 60, 90, 9, 15, 13];
 
+let count = 0;
+
 for (var i = 0; i < values.length; i++) {
-  if (values[i] >= 0 && values[i] <= 20) {
-    console.log(values[i]);
+  if (values[i] >= 10 && values[i] <= 20) {
+    count++;
   }
 }
+
+console.log(count);
