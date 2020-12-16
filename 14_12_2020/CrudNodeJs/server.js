4b0d361add9e955b276c7e8dd443f8ebc4151495
Lucas Rosa <lucassantosrosa51@gmail.com>
2020-12-16 13:01:19 -0300

    Adiciona cors

--- 14_12_2020/CrudNodeJs/server.js
+++ 14_12_2020/CrudNodeJs/server.js
@@ -1,30 +1,22 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 require("dotenv/config"); //* Necessário para ler variaveis de ambiente .env
 const helpers = require("./helpers/index"); //* Arquivo de utilidades
 const routes = require("./src/routes");
 const app = express();
-//  const MongoClient = require("mongodb").MongoClient;
-
+const cors = require('cors');
+// Gera URL do banco
 const uri = helpers.getUrl(
   process.env.USER,
   process.env.PASSWORD,
   process.env.DATABASE
 );
 
+// Conecta com o banco
 helpers.getDatabase(app, uri);
 
-/* //! Conexão com o banco mongo
-global.connection = MongoClient.connect(uri, { useUnifiedTopology: true }, (err, client) => {
-  if (err) return console.log(err);
-  db = client.db(process.env.DATABASE);
-
-  app.listen(3000, () => {
-    console.log("Server running on port 3000");
-  });
-}); */
-
-app.use(bodyParser.urlencoded({ extended: true }));
+// Config 
+app.use(cors());
+app.use(express.json());
 
 // Engine de View
 app.set("view engine", "ejs");
